5f8f4ceee8a1f460cf9f2e14eb6bf7c523d9f131
chuhsiu <zzh32224@gmail.com>
2025-06-06 09:48:35 +0800

    Updates

--- src/services/fetchers.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchUsers(duration: number) {
-    const res = await fetch(`/users?duration=${duration}`)
-    if (!res.ok) throw new Error('Network response was not ok')
-    return await res.json()
-  }
